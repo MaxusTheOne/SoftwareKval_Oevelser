608d868bb7ee82b72fcdff58986a8559d3b4cec5
MaxusTheOne <markus@bille.dk>
2026-01-28 15:52:01 +0100

    Create RomanTranslator.py

---
+++ src/RomanTranslator.py
@@ -0,0 +1,19 @@
+
+def translate_roman_to_integer(roman_numeral):
+    roman_to_int_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for integer in reversed(roman_numeral):
+
+
+    return total
