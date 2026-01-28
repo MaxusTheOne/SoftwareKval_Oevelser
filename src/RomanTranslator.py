3d0ea7b6ce32d8aca23b4aeca9ea5c013f29a409
MaxusTheOne <markus@bille.dk>
2026-01-28 17:20:06 +0100

    Roman translation and testing done

--- src/RomanTranslator.py
+++ src/RomanTranslator.py
@@ -1,3 +1,4 @@
+import copy
 
 def translate_roman_to_integer(roman_numeral):
     roman_to_int_map = {
@@ -11,9 +12,41 @@ def translate_roman_to_integer(roman_numeral):
     }
 
     total = 0
-    prev_value = 0
+    length = len(roman_numeral)
 
-    for integer in reversed(roman_numeral):
+    for i in reversed(range(length)):
+        if roman_numeral[i] not in roman_to_int_map:
+            raise TypeError
+        numeral = roman_numeral[i]
+        value = roman_to_int_map[roman_numeral[i]]
 
+        if i + 1 < length and numeral in {"V", "L", "D"}:
+            if numeral == roman_numeral[i+1]:
+                raise ValueError("Not allowed to repeat V L D. Got: " + numeral + " and " + roman_numeral[i-1])
+        elif i + 3 <= length:
+
+            y = i
+            repeat_count = 0
+            while y < length:
+                if roman_numeral[y] != numeral:
+                    break
+                else:
+                    y += 1
+                    repeat_count += 1
+                    if repeat_count > 3:
+                        raise ValueError("Not allowed to repeat " + numeral + " more than 3 times.")
+
+        if i < length -1:
+            if value < roman_to_int_map[roman_numeral[i+1]]:
+                if numeral in {"I", "X", "C"}:
+                    total -= value
+                else:
+                    raise ValueError("Not allowed to subtract with " + numeral)
+                continue
+
+        total += value
+
+    if total >= 4000:
+        raise ValueError("Value can't be greater than 4000")
 
     return total
--- tests/Roman_test.py
+++ tests/Roman_test.py
@@ -2,22 +2,30 @@ import unittest
 
 import pytest
 
+from src.RomanTranslator import translate_roman_to_integer
 
-class RomanCalcTester(unittest.TestCase):
-    @pytest.mark.parametrize("input,expected", [
-        ("II", 2),
-        ("IV", 4),
-        ("MDCCCLXVII", 1867),
-        ("XCIV", 94),
-        ("MMMCMXCIX", 3999),
 
-        ("XXXX", None),
-        ("VV", None),
-        ("LM", None),
-        ("MMMCMXCXX", None),
-    ])
-    def test_something(self):
-        self.assertEqual(roman_translation(input), expected)  # add assertion here
+@pytest.mark.parametrize("roman,expected", [
+    ("II", 2),
+    ("IV", 4),
+    ("MDCCCLXVII", 1867),
+    ("XCIV", 94),
+    ("MMMCMXCIX", 3999),
+
+
+])
+def test_roman_calc(roman, expected):
+    assert translate_roman_to_integer(roman) == expected
+
+@pytest.mark.parametrize("roman, error_type", [
+    ("XXXX", ValueError),
+    ("VV", ValueError),
+    ("LM", ValueError),
+    ("MMMCMXCXX", ValueError),
+])
+def test_invalid_input(roman, error_type):
+    with pytest.raises(error_type):
+        translate_roman_to_integer(roman)
 
 
 if __name__ == '__main__':
